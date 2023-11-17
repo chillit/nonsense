a467291bb27c6d0dafd7ed83a9b51e1314697603
chillit <121033253+chillit@users.noreply.github.com>
2023-11-17 20:22:05 +0600

    fewfwe

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:nonsense/people_list.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();  await Firebase.initializeApp(
       options: FirebaseOptions(
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const PeopleList(),
     );
   }
 }
